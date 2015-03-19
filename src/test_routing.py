537c142abd1d4ceef4e508159ab54ae635347c7c
Staņislavs Rogozins <stanislavs.rogozins@gmail.com>
2015-03-20 00:07:35 +0200

    Added some basic corner case tests for graph_region_jumps()

--- src/test_routing.py
+++ src/test_routing.py
@@ -1,8 +1,15 @@
 import unittest
-from mock import Mock
+from mock import Mock, patch
 
-# Mock dbclient module
+# Mocks, etc.
 mock_dbclient = Mock()
+
+
+class FakeRJ:
+    def __init__(self, fromID, toID):
+        self.fromID = fromID
+        self.toID = toID
+
 
 import sys
 sys.modules['dbclient'] = mock_dbclient
@@ -10,18 +17,36 @@ sys.modules['dbclient'] = mock_dbclient
 import routing
 
 
-class TestRoutingtMethods(unittest.TestCase):
-    def test_graph_region_jump(self):
+class TestGraphMethods(unittest.TestCase):
+
+    @patch('dbclient.api.list_region_jumps')
+    def test_graph_rj_with_no_rjs(self, mock_list_region_jumps):
+        mock_list_region_jumps.return_value = []
+        expected_res = {}
+
         res = routing.graph_region_jumps()
+        mock_list_region_jumps.asset_called_with()
+        self.assertEqual(res, expected_res)
+
+    @patch('dbclient.api.list_region_jumps')
+    def test_graph_rj_with_2_connected_rjs(self, mock_list_region_jumps):
+        mock_list_region_jumps.return_value = [FakeRJ(1, 2), FakeRJ(2, 1)]
+        expected_res = {1: [2], 2: [1]}
+
+        res = routing.graph_region_jumps()
+        mock_list_region_jumps.asset_called_with()
+        self.assertEqual(res, expected_res)
+
+
+class TestRoutingMethods(unittest.TestCase):
 
     def test_shortest_routes_with_same_system(self):
-        res = routing.shortest_routes('SYSTEM', 'SYSTEM');
+        res = routing.shortest_routes('SYSTEM', 'SYSTEM')
         self.assertEqual(res, ['SYSTEM', 'SYSTEM'])
 
     def test_shortest_region_routes_with_same_region(self):
-        res = routing.shortest_routes('REGION', 'REGION');
+        res = routing.shortest_routes('REGION', 'REGION')
         self.assertEqual(res, ['REGION', 'REGION'])
 
 if __name__ == '__main__':
     unittest.main()
-
