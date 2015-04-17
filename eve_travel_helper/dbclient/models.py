b33d95644cd22eaf5a8e4149c9920dbc3eb9af45
Staņislavs Rogozins <stanislavs.rogozins@gmail.com>
2015-04-17 22:48:25 +0300

    Provided all modules with as_dict() method for easier conversion to JSON. New-style class, because classic not supported

--- eve_travel_helper/dbclient/models.py
+++ eve_travel_helper/dbclient/models.py
@@ -3,10 +3,27 @@
 Module contains mapping between SDE database tables and python classes
 
 """
+from copy import deepcopy
 from sqlalchemy import Column, Integer, String, Boolean, REAL
 from sqlalchemy.ext.declarative import declarative_base
 
-_Base = declarative_base()
+
+class _DictableBase(object):
+    """Pass this class as `cls` argument to declarative base to add method for
+    getting dict representation to all child classes
+
+    """
+
+    def as_dict(self):
+        """Return a dictionary of the entry's attribute-value pair, obeying
+        custom propery names
+
+        """
+        d = deepcopy(self.__dict__)
+        d.pop('_sa_instance_state')
+        return d
+
+_Base = declarative_base(cls=_DictableBase)
 
 
 class System(_Base):
@@ -38,6 +55,11 @@ class System(_Base):
     radius = Column(REAL)
     sunTypeID = Column(Integer)
     securityClass = Column(String(length=2))
+
+    def as_dict(self):
+        d = deepcopy(self.__dict__)
+        d.pop('_sa_instance_state')
+        return d
 
 
 class Region(_Base):
